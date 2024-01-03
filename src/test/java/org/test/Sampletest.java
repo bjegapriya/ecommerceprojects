d1fffbfd07b1fee6a928950dc62ea19369821580
jegapriya <jegapriyab1996@gmail.com>
2024-01-03 22:51:51 +0530

    src file

---
+++ src/test/java/org/test/Sampletest.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Sampletest {
+
+	
+	@Test
+	private void test() {
+		System.out.println("T1");
+		System.out.println("T2");
+	System.out.println("T3");
+	}
+	
+}
